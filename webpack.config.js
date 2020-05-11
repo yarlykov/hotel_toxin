0efb7fac58c3148642d502a180dcb7e9815bbda4
Roman Yarlykov <yarlykov92@gmail.com>
2020-05-11 14:41:41 +0700

    add pug

--- webpack.config.js
+++ webpack.config.js
@@ -5,40 +5,48 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
-    main: "./src/colors_type/index.js",
+    main: './src/colors_type/index.js',
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
   },
 
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true
+        }
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
         exclude: [/fonts/],
       },
       {
         test: /\.(ttf|woff|svg|eot)$/,
-        loader: "file-loader",
+        loader: 'file-loader',
         options: {
-          name: "[name].[ext]",
-          outputPath: "fonts/",
+          name: '[name].[ext]',
+          outputPath: 'fonts/',
         },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/colors_type/index.html",
+      template: './src/pug/pages/index.pug',
+      filename: 'index.html'
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
+      filename: '[name].css',
     }),
     new CleanWebpackPlugin(),
   ],
