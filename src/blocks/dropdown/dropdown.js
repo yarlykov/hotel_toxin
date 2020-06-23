73ca65dd99f357fe2cc90a3d12def736618e7e01
Roman Yarlykov <yarlykov92@gmail.com>
2020-06-23 09:50:18 +0700

    add iqDropdown dropdown block(no style)

---
+++ src/blocks/dropdown/dropdown.js
@@ -0,0 +1,47 @@
+import './item-quantity-dropdown.min.css';
+import './item-quantity-dropdown.min.js';
+
+$(document).ready(function () {
+	$(".no-config").iqDropdown({
+		selectionText: "Сколько гостей",
+		plurals: {
+			guests: ["гость","гостя","гостей"],
+			babies: ["младенец", "младенца", "младенцев"],
+		}
+	});
+	
+	$(".custom").iqDropdown({
+		minItems: 1,
+		maxItems: 5,
+		selectionText: "passenger",
+		textPlural: "passengers",
+		onChange: function (id, count, totalItems) {
+			console.log(id, count, totalItems);
+		},
+		beforeDecrement: function (id, itemCount) {
+			if (id === "adult") {
+				return itemCount.adult > itemCount.infant;
+			}
+			return true;
+		},
+		beforeIncrement: function (id, itemCount) {
+			if (id === "infant") {
+				return itemCount.adult > itemCount.infant;
+			}
+			return true;
+		}
+	});
+
+	$(".custom-message").iqDropdown({
+		minItems: 0,
+		onChange: function (id, count, totalItems) {
+			console.log(id, count, totalItems);
+		},
+		setSelectionText: function (itemCount, totalItems) {
+			const items = Object.keys(itemCount)
+				.map(key => itemCount[key])
+				.join(' + ');
+			return items + ' = ' + totalItems;
+		}
+	});
+});
