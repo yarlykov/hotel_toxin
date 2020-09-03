f59a8a0393a9c7e9aacc2431ad8faa908b8f7180
Roman Yarlykov <yarlykov92@gmail.com>
2020-09-03 21:13:18 +0700

    add: air-datepicker

---
+++ src/blocks/air-datepicker/air-datepicker.js
@@ -0,0 +1,25 @@
+import 'air-datepicker/dist/js/datepicker.js';
+import 'air-datepicker/dist/css/datepicker.css';
+
+$('.air-datepicker').datepicker({
+	dateFormat: 'd M',
+	multipleDatesSeparator: ' - ',
+	range: true,
+	clearButton: true,
+	// offset: -10,
+
+
+})
+
+
+$('.text-field__input_hover').datepicker({
+	dateFormat: 'd M',
+	multipleDatesSeparator: ' - ',
+	range: true,
+	clearButton: true,
+	// offset: -10,
+})
+
+
+
+
