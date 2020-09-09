303fd9201b79f77644a17f4c868aee7a1c5f4faf
Roman Yarlykov <yarlykov92@gmail.com>
2020-09-09 12:12:51 +0700

    add: pagination block options, styles and imports

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import './blocks/range-slider/range-slider';
 import './blocks/dropdown/dropdown';
 import './blocks/mask/mask'
 import './blocks/like/like'
-import './blocks/paginaton/pagination'
+import './blocks/pagination/pagination'
 
 import './main.scss';
