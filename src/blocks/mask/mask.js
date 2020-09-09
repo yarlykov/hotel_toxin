9e0365cc0b26de5d436df5dd41a60c9d9cb05d24
Roman Yarlykov <yarlykov92@gmail.com>
2020-09-09 21:08:06 +0700

    refactor: inputmask

--- src/blocks/mask/mask.js
+++ src/blocks/mask/mask.js
@@ -10,11 +10,13 @@ day < 10 ? day = '0' + day : day
 month < 10 ? month = '0' + month : month
 todayDate = `${day}/${month}/${year}`
 
-const maskDateSelector = document.querySelector('.masked__text-field_date')
+const maskDateSelector = document.querySelector('.js-masked__text-field')
 
 Inputmask('datetime', {
     inputFormat: 'dd.mm.yyyy',
     placeholder: '__.__.____',
     min: '01/01/1900',
-    max: todayDate
+    max: todayDate,
+    showMaskOnHover: false,
+    showMaskOnFocus: false,
   }).mask(maskDateSelector);
