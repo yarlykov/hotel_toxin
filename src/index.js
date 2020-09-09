c83731536e936953c1f0b54485c9e72733915e0b
Roman Yarlykov <yarlykov92@gmail.com>
2020-09-09 10:50:43 +0700

    add: pagination plugin

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import './blocks/range-slider/range-slider';
 import './blocks/dropdown/dropdown';
 import './blocks/mask/mask'
 import './blocks/like/like'
+import './blocks/paginaton/pagination'
 
 import './main.scss';
