b13c3b37681150bf2358213f7231090610d73f3f
Roman Yarlykov <yarlykov92@gmail.com>
2020-09-24 16:04:09 +0700

    fix: pagination errors
    errors caused by plugin operation on different pages

--- src/blocks/pagination/pagination.js
+++ src/blocks/pagination/pagination.js
@@ -1,14 +1,17 @@
 import 'paginationjs/dist/pagination.js'
 import 'paginationjs/dist/pagination.css'
 
-$('.js-pagination').pagination({
-  dataSource: [1, 2, 3, 4, 5],
-  pageSize: 0.35,
-  pageRange: 1,
-  showNavigator: true,
-  formatNavigator: '<span><%= currentPage %></span> – 12 из 100+ вариантов аренды',
-  showPrevious: false,
-  showNext: true,
-  nextText: '<span class="material-icons js-pagination__next-button">arrow_forward</span>',
-  autoHideNext: true,
+const paginationMainNode = $('.js-pagination')
+paginationMainNode.each(function(){
+  $(this).pagination({
+    dataSource: [1, 2, 3, 4, 5],
+    pageSize: 0.35,
+    pageRange: 1,
+    showNavigator: true,
+    formatNavigator: '<span><%= currentPage %></span> – 12 из 100+ вариантов аренды',
+    showPrevious: false,
+    showNext: true,
+    nextText: '<span class="material-icons js-pagination__next-button">arrow_forward</span>',
+    autoHideNext: true,
+  })
 })
