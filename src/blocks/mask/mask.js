17eaa1294d097ae1bc37b51ba0e5e65d516827d6
Roman Yarlykov <yarlykov92@gmail.com>
2020-09-24 16:16:47 +0700

    fix: input mask errors
    errors caused by plugin operation on different pages

--- src/blocks/mask/mask.js
+++ src/blocks/mask/mask.js
@@ -10,13 +10,15 @@ day < 10 ? day = '0' + day : day
 month < 10 ? month = '0' + month : month
 todayDate = `${day}/${month}/${year}`
 
-const maskDateSelector = document.querySelector('.js-masked__text-field')
+const maskDateSelector = document.querySelectorAll('.js-masked__text-field')
 
-Inputmask('datetime', {
-    inputFormat: 'dd.mm.yyyy',
-    placeholder: '__.__.____',
-    min: '01/01/1900',
-    max: todayDate,
-    showMaskOnHover: false,
-    showMaskOnFocus: false,
-  }).mask(maskDateSelector);
+maskDateSelector.forEach(selector => {
+  Inputmask('datetime', {
+      inputFormat: 'dd.mm.yyyy',
+      placeholder: '__.__.____',
+      min: '01/01/1900',
+      max: todayDate,
+      showMaskOnHover: false,
+      showMaskOnFocus: false,
+    }).mask(selector);
+})
