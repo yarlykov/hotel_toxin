5a33a3650a16bdd3dfa2c4377695ce9105428cf2
Roman Yarlykov <yarlykov92@gmail.com>
2020-10-14 22:49:08 +0700

    add: hide navigation dropdown when click outside

--- src/blocks/navigation/navigation.js
+++ src/blocks/navigation/navigation.js
@@ -1,7 +1,14 @@
-const mainNavigationDropdownNode = $('.navigation__item_dropdown')
+const mainNavigationDropdownNode = $('.navigation__item_dropdown');
 
 mainNavigationDropdownNode.each(function(){
   $(this).on('click', function(){
-    $(this).toggleClass('navigation__dropdown_open')
+    $(this).toggleClass('navigation__dropdown_open');
+    $(this).find('.navigation__dropdown').toggle();
   })
 })
+
+$(document).on('mouseup', function (e) {
+  if (mainNavigationDropdownNode.has(e.target).length === 0) {
+    mainNavigationDropdownNode.find('.navigation__dropdown').hide();
+  }
+});
