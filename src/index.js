2b81ebb0cf2812a62b214547164866492b689551
Roman Yarlykov <yarlykov92@gmail.com>
2020-10-19 22:05:42 +0700

    rename: folder blocks to components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import './blocks/air-datepicker/air-datepicker'
-import './blocks/range-slider/range-slider';
-import './blocks/dropdown/dropdown';
-import './blocks/mask/mask'
-import './blocks/like/like'
-import './blocks/pagination/pagination'
-import './blocks/checkbox/checkbox'
-import './blocks/header/header'
-import './blocks/navigation/navigation'
+import './components/air-datepicker/air-datepicker'
+import './components/range-slider/range-slider';
+import './components/dropdown/dropdown';
+import './components/mask/mask'
+import './components/like/like'
+import './components/pagination/pagination'
+import './components/checkbox/checkbox'
+import './components/header/header'
+import './components/navigation/navigation'
 
 import './main.scss';
