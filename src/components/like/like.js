62f857268d6f48edfabc6ffb043437ce85848952
Roman Yarlykov <yarlykov92@gmail.com>
2020-11-07 16:25:59 +0700

    fix: material icons font optimization

--- src/components/like/like.js
+++ src/components/like/like.js
@@ -6,13 +6,20 @@ $(document).ready(function(){
     const likesCount = $likesCounter.text()
   
     $(this).toggleClass('like__disable_border')
-    $('.like__icon', this).toggleClass('like__disable')
     $('.like__text', this).toggleClass('like__disable')
   
     const isClicked = $(this).hasClass('like__disable_border')
-    $likesCounter.text((isClicked) ? +likesCount - 1 : +likesCount + 1)
-  
-    $('.like__icon', this).text((isClicked) ? 'favorite_border' : 'favorite') 
+    $likesCounter.text((isClicked) ? 
+    +likesCount - 1 : 
+    +likesCount + 1)
+    
+    let $likeIcon = $('.like__icon', this)
+
+    $likeIcon.hasClass('like__icon_favorite') ? 
+    $likeIcon.removeClass('like__icon_favorite') : 
+    $likeIcon.addClass('like__icon_favorite')
+
+    $likeIcon.hasClass('like__icon_favorite_border') ? $likeIcon.removeClass('like__icon_favorite_border') : $likeIcon.addClass('like__icon_favorite_border')
   }
   
   $likeButtons.on('click', clickHandler)
