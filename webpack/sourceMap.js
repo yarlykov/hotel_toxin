611333ccd8c2b386b5374ccf93c0ac83ddb29112
Roman Yarlykov <yarlykov92@gmail.com>
2020-11-11 12:53:07 +0700

    add: sourceMap in webpack config

---
+++ webpack/sourceMap.js
@@ -0,0 +1,12 @@
+const webpack = require('webpack')
+
+module.exports = function() {
+  return {
+    devtool: 'cheap-module-eval-source-map',
+    plugins: [
+      new webpack.SourceMapDevToolPlugin({
+        filename: '[file].map'
+      })
+    ]
+  }
+}
