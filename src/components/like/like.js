759c82f1c62f5dac0a845b6cc1ef6c2b4f53982c
Roman Yarlykov <yarlykov92@gmail.com>
2020-11-24 17:44:40 +0700

    refactor: eslint airbnb rules

--- src/components/like/like.js
+++ src/components/like/like.js
@@ -1,26 +1,32 @@
-$(document).ready(function(){
-  const $likeButtons = $('.like__button')
-  
+$(document).ready(() => {
+  const $likeButtons = $('.like__button');
+
   const clickHandler = function () {
-    const $likesCounter = $('.like__text', this)
-    const likesCount = $likesCounter.text()
-  
-    $(this).toggleClass('like__disable_border')
-    $('.like__text', this).toggleClass('like__disable')
-  
-    const isClicked = $(this).hasClass('like__disable_border')
-    $likesCounter.text((isClicked) ? 
-    +likesCount - 1 : 
-    +likesCount + 1)
-    
-    let $likeIcon = $('.like__icon', this)
+    const $likesCounter = $('.like__text', this);
+    const likesCount = $likesCounter.text();
 
-    $likeIcon.hasClass('like__icon_favorite') ? 
-    $likeIcon.removeClass('like__icon_favorite') : 
-    $likeIcon.addClass('like__icon_favorite')
+    $(this).toggleClass('like__disable_border');
+    $('.like__text', this).toggleClass('like__disable');
 
-    $likeIcon.hasClass('like__icon_favorite_border') ? $likeIcon.removeClass('like__icon_favorite_border') : $likeIcon.addClass('like__icon_favorite_border')
-  }
-  
-  $likeButtons.on('click', clickHandler)
-})
+    const isClicked = $(this).hasClass('like__disable_border');
+    $likesCounter.text((isClicked)
+      ? +likesCount - 1
+      : +likesCount + 1);
+
+    const $likeIcon = $('.like__icon', this);
+
+    if ($likeIcon.hasClass('like__icon_favorite')) {
+      $likeIcon.removeClass('like__icon_favorite');
+    } else {
+      $likeIcon.addClass('like__icon_favorite');
+    }
+
+    if ($likeIcon.hasClass('like__icon_favorite_border')) {
+      $likeIcon.removeClass('like__icon_favorite_border');
+    } else {
+      $likeIcon.addClass('like__icon_favorite_border');
+    }
+  };
+
+  $likeButtons.on('click', clickHandler);
+});
