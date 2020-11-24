99004707a99d7547d4be8b60e93d4487c3524982
Roman Yarlykov <yarlykov92@gmail.com>
2020-11-24 17:40:19 +0700

    refactor: eslint airbnb rules

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,7 @@
-const { each } = require("jquery")
+const headerMenuBurgerNode = $('.js-header__menu-burger');
 
-const headerMenuBurgerNode = $('.js-header__menu-burger')
-
-headerMenuBurgerNode.each(function(){
-  $(this).on('click', function(){
-    $(this).toggleClass('header__active')
-  })
-})
+headerMenuBurgerNode.each(function () {
+  $(this).on('click', function () {
+    $(this).toggleClass('header__active');
+  });
+});
