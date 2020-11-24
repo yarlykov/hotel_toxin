01123ef6b2a6c67a85ee2808a15241ca80d8b548
Roman Yarlykov <yarlykov92@gmail.com>
2020-11-24 12:11:08 +0700

    refactor: eslint airbnb rules

--- src/components/chart/chart.js
+++ src/components/chart/chart.js
@@ -1,53 +1,56 @@
 const captionsList = document.querySelectorAll('.legend__item');
 const unitsList = document.querySelectorAll('.unit');
-let sumOfVote = document.querySelector('.chart__sum-of-vote')
+const sumOfVote = document.querySelector('.chart__sum-of-vote');
 
-const getTemplate = (number, color) => {
-  return `
+const getTemplate = (number, color) => `
   <g class="chart__sum-of-vote" fill=${color}>
     <text class="sum-of-vote__number" text-anchor="middle" x="50%" y="49%">${number}</text>
     <text class="sum-of-vote__text" text-anchor="middle" x="50%" y="67%">голосов</text>
   </g>
-  ` 
-}
+  `;
 
-const sumOfVoteColors = function(index) {
+const sumOfVoteColors = (index) => {
   switch (index) {
     case 0:
-      return sumOfVote.innerHTML = getTemplate(500, '#FFE39C');
+      sumOfVote.innerHTML = getTemplate(500, '#FFE39C');
+      break;
     case 1:
-      return sumOfVote.innerHTML = getTemplate(260, '#BC9CFF');
+      sumOfVote.innerHTML = getTemplate(260, '#BC9CFF');
+      break;
     case 2:
-      return sumOfVote.innerHTML = getTemplate(260, '#6FCF97');
+      sumOfVote.innerHTML = getTemplate(260, '#6FCF97');
+      break;
     default:
       return index;
   }
-}
+  return index;
+};
 
-captionsList.forEach(function (item, index) {
+captionsList.forEach((item, index) => {
+  let changeIndex = index;
   if (index === 1) {
-    index += 1
+    changeIndex += 1;
   } else if (index === 2) {
-    index--
+    changeIndex -= 1;
   }
-  
-  item.addEventListener('mouseover', function () {
-    sumOfVoteColors(index)
-    unitsList[index].classList.add('js-legend-hovered');
+
+  item.addEventListener('mouseover', () => {
+    sumOfVoteColors(changeIndex);
+    unitsList[changeIndex].classList.add('js-legend-hovered');
   });
 
-  item.addEventListener('mouseout', function () {
-    unitsList[index].classList.remove('js-legend-hovered');
+  item.addEventListener('mouseout', () => {
+    unitsList[changeIndex].classList.remove('js-legend-hovered');
   });
 });
 
-unitsList.forEach(function (item, index) {  
-  item.addEventListener('mouseover', function () {
+unitsList.forEach((item, index) => {
+  item.addEventListener('mouseover', () => {
     sumOfVoteColors(index);
     unitsList[index].classList.add('js-legend-hovered');
   });
 
-  item.addEventListener('mouseout', function () {
+  item.addEventListener('mouseout', () => {
     unitsList[index].classList.remove('js-legend-hovered');
   });
 });
