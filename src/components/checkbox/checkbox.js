5ea986902f5184d6235915691cd2c8e78af7037f
Roman Yarlykov <yarlykov92@gmail.com>
2020-11-24 12:17:24 +0700

    refactor: eslint airbnb rules

--- src/components/checkbox/checkbox.js
+++ src/components/checkbox/checkbox.js
@@ -1,16 +1,12 @@
-$(document).ready(function(){
+const checkboxDropdown = $('.js-checkbox .checkbox__header');
 
-	const checkboxDropdown = $('.js-checkbox .checkbox__header')
-	
-	checkboxDropdown.on('click', function(){
-		const arrowDropdown = $(this).find('.js-checkbox__header_arrow')
-		const itemDropdown = $(this).parent().find('.checkbox__item')
+checkboxDropdown.on('click', function arrowCheckbox() {
+  const arrowDropdown = $(this).find('.js-checkbox__header_arrow');
+  const itemDropdown = $(this).parent().find('.checkbox__item');
 
-		if (itemDropdown.length){
-			itemDropdown.toggle()
-			arrowDropdown.toggleClass('checkbox__header_arrow-up')
-			arrowDropdown.toggleClass('checkbox__header_arrow-down')
-		}
-	})
-
-})
+  if (itemDropdown.length) {
+    itemDropdown.toggle();
+    arrowDropdown.toggleClass('checkbox__header_arrow-up');
+    arrowDropdown.toggleClass('checkbox__header_arrow-down');
+  }
+});
