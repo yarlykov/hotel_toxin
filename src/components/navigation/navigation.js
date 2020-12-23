fa4955a74662fbb75fc1c1b30996a3c98a13fc3e
Roman Yarlykov <yarlykov92@gmail.com>
2020-12-23 19:24:10 +0700

    add: methods in NavigationDropdown class
    _init
    _setup
    keydownHandler
    onDropdownEnterPress
    onDropdownEscapePress
    toggleDropdown
    closeDropdown

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -17,18 +17,51 @@ class NavigationDropdown {
   constructor(selector) {
     this.$mainNode = selector;
 
-    this.init();
+    this._init();
+    this._setup();
   }
 
-  init() {
-    this.itemDropdown = this.$mainNode.querySelector('.navigation__dropdown');
+  _init() {
+    this.$itemDropdown = this.$mainNode.querySelector('.navigation__dropdown');
+    this.$visibilityHiddenClass = 'navigation__dropdown_visibility-hidden';
+  }
+
+  _setup() {
     this.clickHandler();
+    this.keydownHandler();
   }
 
   clickHandler() {
     this.$mainNode.addEventListener('click', () => {
-      this.itemDropdown.classList.toggle('navigation__dropdown_visibility-hidden');
+      this.toggleDropdown();
     });
+  }
+
+  keydownHandler() {
+    this.$mainNode.addEventListener('keydown', (event) => {
+      this.onDropdownEnterPress(event);
+      this.onDropdownEscapePress(event);
+    });
+  }
+
+  onDropdownEnterPress(event) {
+    if (event.code === 'Enter') {
+      this.toggleDropdown();
+    }
+  }
+
+  onDropdownEscapePress(event) {
+    if (event.code === 'Escape') {
+      this.closeDropdown();
+    }
+  }
+
+  toggleDropdown() {
+    this.$itemDropdown.classList.toggle(this.$visibilityHiddenClass);
+  }
+
+  closeDropdown() {
+    this.$itemDropdown.classList.add(this.$visibilityHiddenClass);
   }
 }
 
