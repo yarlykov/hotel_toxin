5d7d398e75cca50d8726299615f2ae96fe112d83
Roman Yarlykov <yarlykov92@gmail.com>
2021-09-02 16:50:06 +0700

    added: stylelint #19
    "stylelint-config-standard"
    Turns on additional rules to enforce the common stylistic conventions found within a handful of CSS styleguides, including: The Idiomatic CSS Principles, Google's CSS Style Guide, Airbnb's Styleguide, and @mdo's Code Guide.

---
+++ .stylelintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-rational-order",
+    "stylelint-prettier/recommended"
+  ],
+  plugins: ["stylelint-order", "stylelint-scss"],
+  rules: {
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true,
+  }
+}
