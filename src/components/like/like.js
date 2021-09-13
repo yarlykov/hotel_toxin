f37b65032441e195b02da5c76f1e46bf8277a406
Roman Yarlykov <yarlykov92@gmail.com>
2021-09-13 12:51:58 +0700

    refactor: removed position absolute in like block #20

--- src/components/like/like.js
+++ src/components/like/like.js
@@ -18,30 +18,30 @@ class Like {
   }
 
   buttonToggle() {
-    this.root.classList.toggle('like_inactive-border');
+    this.root.classList.toggle('like_inactive');
     this.text.classList.toggle('like__text_inactive');
   }
 
   isEnabled() {
-    return this.heart.classList.contains('like__icon_heart');
+    return this.heart.classList.contains('like__icon_type_heart');
   }
 
   iconToggle() {
     if (this.isEnabled()) {
-      this.heart.classList.remove('like__icon_heart');
-      this.heart.classList.add('like__icon_empty-heart');
+      this.heart.classList.remove('like__icon_type_heart');
+      this.heart.classList.add('like__icon_type_empty-heart');
     } else {
-      this.heart.classList.remove('like__icon_empty-heart');
-      this.heart.classList.add('like__icon_heart');
+      this.heart.classList.remove('like__icon_type_empty-heart');
+      this.heart.classList.add('like__icon_type_heart');
     }
   }
 
   counter() {
     const currentValue = Number(this.text.textContent);
     if (!this.isEnabled()) {
-      this.text.innerHTML = this.decrement(currentValue);
+      this.text.innerHTML = Like.decrement(currentValue);
     } else {
-      this.text.innerHTML = this.increment(currentValue);
+      this.text.innerHTML = Like.increment(currentValue);
     }
   }
 
