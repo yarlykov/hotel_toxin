037052e9c2ec18e03609fc517c069e903071776d
Roman Yarlykov <yarlykov92@gmail.com>
2021-09-15 20:45:56 +0700

    fixed: error when starting dev server #23

--- webpack/devServer.js
+++ webpack/devServer.js
@@ -1,16 +1,8 @@
-const path = require('path');
-
 module.exports = function () {
   return {
     devServer: {
-      static: {
-        directory: path.join(__dirname, '../dist'),
-        staticOptions: {
-          index: 'start-page.html',
-        },
-      },
       port: 8081,
-      open: true,
+      open: ['/start-page.html'],
     },
   };
 };
