2ac929f42151fbc3a1c387c4833d1b31eec37a18
Roman Yarlykov <yarlykov92@gmail.com>
2021-09-21 15:50:02 +0700

    fixed: all pie-chart bugs #24

--- src/components/chart/chart.js
+++ src/components/chart/chart.js
@@ -6,44 +6,144 @@ class Chart {
   }
 
   init() {
-    this.chartBody = this.root.querySelector('[data-id="body"]');
-    this.sumOfVote = this.root.querySelector('[data-id="sum-of-vote"]');
+    const { votes } = this.options;
 
-    this.root.addEventListener('mouseover', this.handleChartMouseover.bind(this));
-    this.root.addEventListener('mouseout', this.handleChartMouseout.bind(this));
+    this.root.insertAdjacentHTML('afterbegin', Chart.getMainTemplate());
+    this.sumOfAllVotes = votes.reduce((acc, item) => item.votesAmount + acc, 0);
+
+    this.addChartElements();
+    this.addLegendList();
+    this.addSumOfVotes();
+    this.addEventListeners();
   }
 
-  handleChartMouseover({ target }) {
+  addChartElements() {
+    this.chartBody = this.root.querySelector('[data-id="body"]');
+    const { votes } = this.options;
+    const PI = 3.14;
+    const radius = 15.9;
+    const circumference = Math.ceil(2 * PI * radius);
+    let segmentLength = circumference;
+
+    votes.forEach((vote) => {
+      const { votesAmount = 1 } = vote;
+      const strokeDasharray = ((votesAmount * circumference) / this.sumOfAllVotes);
+      segmentLength -= strokeDasharray;
+      const strokeDashoffset = segmentLength * -1;
+      this.chartBody.insertAdjacentHTML(
+        'afterbegin',
+        Chart.getChartBodyTemplate(vote.type, strokeDasharray, strokeDashoffset),
+      );
+    });
+
+    votes.forEach((vote) => {
+      this.chartBody.insertAdjacentHTML(
+        'beforeend',
+        Chart.getLinearGradientTemplate(vote.type, vote.firstStopColor, vote.secondStopColor),
+      );
+    });
+
+    this.chartBody.insertAdjacentHTML('beforeend', Chart.getSumOfVotesTemplate());
+  }
+
+  addLegendList() {
+    const { votes } = this.options;
+    const legend = this.root.querySelector('[data-id="chart-legend"]');
+
+    votes.forEach((vote) => {
+      const { firstStopColor, secondStopColor, type } = vote;
+      legend.insertAdjacentHTML('beforeend', Chart.getLegendItemTemplate(vote.type, vote.name));
+      const item = legend.querySelector(`[data-id="item-point-${type}"]`);
+      item.style.background = `-webkit-linear-gradient(${firstStopColor}, ${secondStopColor})`;
+    });
+  }
+
+  addSumOfVotes() {
+    this.sumOfVote = this.chartBody.querySelector('[data-id="sum-of-vote"]');
+    this.sumOfVote.innerHTML = Chart.getSumOfVotesTemplate(this.sumOfAllVotes, '#919191');
+  }
+
+  addEventListeners() {
+    this.root.addEventListener('mouseover', this.handleChartFocus.bind(this));
+    this.root.addEventListener('mouseout', this.handleChartBlur.bind(this));
+    this.root.addEventListener('focusin', this.handleChartFocus.bind(this));
+    this.root.addEventListener('focusout', this.handleChartBlur.bind(this));
+  }
+
+  handleChartFocus({ target }) {
     const { type } = target.dataset;
 
     if (type) {
-      this.sumOfVote.innerHTML = this.getTemplate(type);
+      const { votes } = this.options;
+      votes.forEach((vote) => {
+        if (vote.type === type) {
+          const template = Chart.getSumOfVotesTemplate(vote.votesAmount, vote.firstStopColor);
+          this.sumOfVote.innerHTML = template;
+        }
+      });
+
       const currentCircle = this.chartBody.querySelector(`[data-type=${type}]`);
       if (currentCircle) currentCircle.classList.add('chart__circle_focused');
     }
   }
 
-  handleChartMouseout({ target }) {
+  handleChartBlur({ target }) {
     const { type } = target.dataset;
     const currentCircle = this.chartBody.querySelector(`[data-type=${type}]`);
+    this.sumOfVote.innerHTML = Chart.getSumOfVotesTemplate(this.sumOfAllVotes, '#919191');
     if (currentCircle) currentCircle.classList.remove('chart__circle_focused');
   }
 
-  getTemplate(type) {
-    const { votes } = this.options;
-    const colors = {
-      sumptuously: '#FFE39C',
-      good: '#6FCF97',
-      satisfactorily: '#BC9CFF',
-      disappointed: '#000000',
-    };
+  static getMainTemplate() {
+    return `
+      <div class="chart__doughnut">
+        <svg class="chart__body" viewBox="0 0 34 30" data-id="body"></svg>
+      </div>
+      <div class="chart__legend" data-id="chart-legend">
+        <ul class="chart__legend-list" data-id="chart-legend"></ul>
+      </div>
+    `;
+  }
+
+  static getLinearGradientTemplate(type = '', firstStopColor = '', secondStopColor = '') {
+    return `
+      <linearGradient id="${type}" gradientTransform="rotate(90)">
+        <stop offset="0" stop-color=${firstStopColor}></stop>
+        <stop offset="1" stop-color=${secondStopColor}></stop>
+      </linearGradient>
+    `;
+  }
+
+  static getChartBodyTemplate(type = '', strokeDasharray = 0, strokeDashoffset = 0) {
+    let strokeWithIntervals = 0;
+    if (strokeDasharray !== 0) {
+      strokeWithIntervals = strokeDasharray - 1;
+    }
 
     return `
-      <g class="chart__sum-of-vote" fill=${colors[type]}>
-        <text class="chart__number" text-anchor="middle" x="50%" y="49%">${votes[type]}</text>
+      <circle class="chart__circle" r="15.9" cx="50%" cy="50%" stroke="url(#${type})"
+      data-id="circle" data-type=${type} stroke-dasharray="${strokeWithIntervals} 100"
+      stroke-dashoffset="${strokeDashoffset}">
+      </circle>
+    `;
+  }
+
+  static getSumOfVotesTemplate(numberOfVotes = 0, textFillColor = '#919191') {
+    return `
+      <g class="chart__sum-of-vote" fill=${textFillColor} data-id="sum-of-vote">
+        <text class="chart__number" text-anchor="middle" x="50%" y="49%">${numberOfVotes}</text>
         <text class="chart__text" text-anchor="middle" x="50%" y="67%">голосов</text>
       </g>
-  `;
+    `;
+  }
+
+  static getLegendItemTemplate(type = '', name = '') {
+    return `
+      <li class="chart__legend-item" data-id="legend-item" data-type=${type} tabindex="0">
+        ${name}
+        <div class="chart__legend-item-point" data-id="item-point-${type}"></div>
+      </li>
+    `;
   }
 }
 
--- src/components/chart/index.js
+++ src/components/chart/index.js
@@ -3,12 +3,36 @@ import Chart from './chart';
 
 const chartElement = document.querySelector('[data-id="chart"]');
 const options = {
-  votes: {
-    sumptuously: 520,
-    good: 260,
-    satisfactorily: 260,
-    disappointed: 0,
-  },
+  votes: [
+    {
+      type: 'sumptuously',
+      name: 'великолепно',
+      votesAmount: 520,
+      firstStopColor: '#FFE39C',
+      secondStopColor: '#FFBA9C',
+    },
+    {
+      type: 'good',
+      votesAmount: 260,
+      name: 'хорошо',
+      firstStopColor: '#6FCF97',
+      secondStopColor: '#66D2EA',
+    },
+    {
+      type: 'satisfactorily',
+      name: 'удовлетворительно',
+      votesAmount: 260,
+      firstStopColor: '#BC9CFF',
+      secondStopColor: '#8BA4F9',
+    },
+    {
+      type: 'disappointed',
+      name: 'разочарован',
+      votesAmount: 0,
+      firstStopColor: '#919191',
+      secondStopColor: '#3D4975',
+    },
+  ],
 };
 
 if (chartElement !== null) new Chart(chartElement, options);
