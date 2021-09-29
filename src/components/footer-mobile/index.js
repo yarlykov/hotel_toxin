b9c4a313acf20b5b89e02a667fe0d09a7aba5e74
Roman Yarlykov <yarlykov92@gmail.com>
2021-09-29 12:14:05 +0700

    fixed: removed block nesting #21

--- src/components/footer-mobile/index.js
+++ src/components/footer-mobile/index.js
@@ -1,1 +1,4 @@
+import 'components/logo';
+import 'components/social';
+
 import './footer-mobile.scss';
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,4 +1,5 @@
 import 'components/footer-mobile';
+import 'components/logo';
 import 'components/links';
 import 'components/heading';
 import 'components/subscribe';
