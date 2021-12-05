06b50306895da565f03359dadb00672ccb83f8ee
Roman Yarlykov <yarlykov92@gmail.com>
2021-12-05 16:42:12 +0700

    like button now has one modifier #27

--- src/components/like/Like.js
+++ src/components/like/Like.js
@@ -6,34 +6,22 @@ class Like {
 
   init() {
     this.text = this.root.querySelector('.js-like__text');
-    this.heart = this.root.querySelector('.js-like__icon');
     if (!this.isEnabled()) this.disabled();
 
     this.root.addEventListener('click', this.handleLikeClick.bind(this));
   }
 
   handleLikeClick() {
-    this.buttonToggle();
-    this.iconToggle();
+    this.root.classList.toggle('like_active');
     this.counter();
   }
 
-  buttonToggle() {
-    this.root.classList.toggle('like_inactive-background');
-    this.text.classList.toggle('like__text_inactive-text');
-  }
-
   isEnabled() {
-    return this.heart.classList.contains('like__icon_active');
+    return this.root.classList.contains('like_active');
   }
 
   disabled() {
-    this.root.classList.add('like_inactive-background');
-    this.text.classList.add('like__text_inactive-text');
-  }
-
-  iconToggle() {
-    this.heart.classList.toggle('like__icon_active');
+    this.root.classList.remove('like_active');
   }
 
   counter() {
